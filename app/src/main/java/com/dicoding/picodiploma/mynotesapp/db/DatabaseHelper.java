aa6fccdfcbf4613655b3b24c2b81a75f3463ea5b
Selly <s3llym@gmail.com>
2019-11-09 11:47:04 +0700

    huhu

---
+++ app/src/main/java/com/dicoding/picodiploma/mynotesapp/db/DatabaseHelper.java
@@ -0,0 +1,40 @@
+package com.dicoding.picodiploma.mynotesapp.db;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+import com.dicoding.picodiploma.mynotesapp.db.DatabaseContract.NoteColumns;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+    public static String DATABASE_NAME = "dbnoteapp";
+    private static final int DATABASE_VERSION = 1;
+    private static final String SQL_CREATE_TABLE_NOTE = String.format("CREATE TABLE %s"
+                    + " (%s INTEGER PRIMARY KEY AUTOINCREMENT," +
+                    " %s TEXT NOT NULL," +
+                    " %s TEXT NOT NULL," +
+                    " %s TEXT NOT NULL)",
+            DatabaseContract.TABLE_NAME,
+            NoteColumns._ID,
+            NoteColumns.TITLE,
+            NoteColumns.DESCRIPTION,
+            NoteColumns.DATE
+    );
+
+    public DatabaseHelper(@Nullable Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        sqLiteDatabase.execSQL(SQL_CREATE_TABLE_NOTE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + DatabaseContract.TABLE_NAME);
+        onCreate(sqLiteDatabase);
+    }
+}
---
+++ app/src/main/java/com/dicoding/picodiploma/mynotesapp/entity/Note.java
@@ -0,0 +1,85 @@
+package com.dicoding.picodiploma.mynotesapp.entity;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class Note implements Parcelable {
+    private int id;
+    private String title;
+    private String description;
+    private String date;
+
+    public Note() {
+    }
+
+    public Note(int id, String title, String description, String date) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.date = date;
+    }
+
+    protected Note(Parcel in) {
+        id = in.readInt();
+        title = in.readString();
+        description = in.readString();
+        date = in.readString();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel parcel, int i) {
+        parcel.writeInt(this.id);
+        parcel.writeString(this.title);
+        parcel.writeString(this.description);
+        parcel.writeString(this.date);
+    }
+
+    public static final Creator<Note> CREATOR = new Creator<Note>() {
+        @Override
+        public Note createFromParcel(Parcel in) {
+            return new Note(in);
+        }
+
+        @Override
+        public Note[] newArray(int size) {
+            return new Note[size];
+        }
+    };
+}
